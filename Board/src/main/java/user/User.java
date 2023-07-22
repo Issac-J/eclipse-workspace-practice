a44e8f9b13d9853e61376bf5632470ffc689d5fd
EggsyJ <fordev0905@gmail.com>
2023-07-22 22:48:40 +0900

    create User class

---
+++ Board/src/main/java/user/User.java
@@ -0,0 +1,42 @@
+package user;
+
+// 데이터를 저장할 수 있는 Class 를 생성 (Java Beans)
+public class User {
+	String userId;
+	String userPw;
+	String userName;
+	String userGender;
+	String userEmail;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+}
